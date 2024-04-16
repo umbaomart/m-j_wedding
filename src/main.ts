2e5c8cd9fef3b0155072d8691196fc14b39b7e9f
dev_Mart <martumbaocommude@gmail.com>
2024-04-17 01:01:56 +0800

    fix ts issue

--- src/main.ts
+++ src/main.ts
@@ -1,113 +1,113 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 
-const swiperGroomsMenThumb = new Swiper('.swiper-groomsThumb', {
-    slidesPerView: 5,
-    freeMode: true,
-    watchSlidesProgress: true,
-})
+// const swiperGroomsMenThumb = new Swiper('.swiper-groomsThumb', {
+//     slidesPerView: 5,
+//     freeMode: true,
+//     watchSlidesProgress: true,
+// })
 
-const swiperGroomsMen = new Swiper('.swiper-groomsMen', {
-    centeredSlides: true,
-    loop: true,
-    spaceBetween: 5,
-    effect: 'fade',
-    fadeEffect: {
-        crossFade: true
-    },
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-    },
-    thumbs: {
-        swiper: swiperGroomsMenThumb
-    }
-})
+// const swiperGroomsMen = new Swiper('.swiper-groomsMen', {
+//     centeredSlides: true,
+//     loop: true,
+//     spaceBetween: 5,
+//     effect: 'fade',
+//     fadeEffect: {
+//         crossFade: true
+//     },
+//     autoplay: {
+//         delay: 5000,
+//         disableOnInteraction: false,
+//     },
+//     thumbs: {
+//         swiper: swiperGroomsMenThumb
+//     }
+// })
 
-const swiperBridesMaidsThumb = new Swiper('.swiper-bridesThumb', {
-    slidesPerView: 5,
-    freeMode: true,
-    watchSlidesProgress: true,
-})
+// const swiperBridesMaidsThumb = new Swiper('.swiper-bridesThumb', {
+//     slidesPerView: 5,
+//     freeMode: true,
+//     watchSlidesProgress: true,
+// })
 
-const swiperBridesMaid = new Swiper('.swiper-bridesMaids', {
-    centeredSlides: true,
-    loop: true,
-    spaceBetween: 5,
-    effect: 'fade',
-    fadeEffect: {
-        crossFade: true
-    },
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-    },
-    thumbs: {
-        swiper: swiperBridesMaidsThumb
-    }
-})
+// const swiperBridesMaid = new Swiper('.swiper-bridesMaids', {
+//     centeredSlides: true,
+//     loop: true,
+//     spaceBetween: 5,
+//     effect: 'fade',
+//     fadeEffect: {
+//         crossFade: true
+//     },
+//     autoplay: {
+//         delay: 5000,
+//         disableOnInteraction: false,
+//     },
+//     thumbs: {
+//         swiper: swiperBridesMaidsThumb
+//     }
+// })
 
 
-const swiperInvitations = new Swiper('.swiper-invitations', {
-    slidesPerView: 1,
-    loop: true,
-    reverseDirection: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-    }
-})
+// const swiperInvitations = new Swiper('.swiper-invitations', {
+//     slidesPerView: 1,
+//     loop: true,
+//     reverseDirection: true,
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     autoplay: {
+//         delay: 5000,
+//         disableOnInteraction: false,
+//     }
+// })
 
-const options = {
-    spaceBetween: 10,
-    loop: true,
-    grabCursor: true,
-    autoplay: {
-        delay: false,
-        disableOnInteraction: false,
-        waitForTransition: true,
-    },
-    speed: 20000,
-    breakpoints: {
-        768: {
-            slidesPerView: 3.5,
-        },
-        767: {
-            slidesPerView: 2,
-        },
-        320: {
-            slidesPerView: 1.7,
-        },
-    },
-}
+// const options = {
+//     spaceBetween: 10,
+//     loop: true,
+//     grabCursor: true,
+//     autoplay: {
+//         delay: false,
+//         disableOnInteraction: false,
+//         waitForTransition: true,
+//     },
+//     speed: 20000,
+//     breakpoints: {
+//         768: {
+//             slidesPerView: 3.5,
+//         },
+//         767: {
+//             slidesPerView: 2,
+//         },
+//         320: {
+//             slidesPerView: 1.7,
+//         },
+//     },
+// }
 
-const swiperGallery = new Swiper('.gallery__slider', options)
-const swiperGallery2 = new Swiper('.gallery__slider2', options)
+// const swiperGallery = new Swiper('.gallery__slider', options)
+// const swiperGallery2 = new Swiper('.gallery__slider2', options)
 
-const swiperGalleryReverse = new Swiper('.gallery__slider-reverse', {
-    direction: 'horizontal', // Set direction to horizontal
-    spaceBetween: 10,
-    loop: true,
-    grabCursor: true,
-    autoplay: {
-        delay: false,
-        disableOnInteraction: false,
-        waitForTransition: true,
-        reverseDirection: true,
-    },
-    speed: 20000,
-    breakpoints: {
-        768: {
-            slidesPerView: 3.5,
-        },
-        767: {
-            slidesPerView: 2,
-        },
-        320: {
-            slidesPerView: 1.7,
-        },
-    },
-})
+// const swiperGalleryReverse = new Swiper('.gallery__slider-reverse', {
+//     direction: 'horizontal', // Set direction to horizontal
+//     spaceBetween: 10,
+//     loop: true,
+//     grabCursor: true,
+//     autoplay: {
+//         delay: false,
+//         disableOnInteraction: false,
+//         waitForTransition: true,
+//         reverseDirection: true,
+//     },
+//     speed: 20000,
+//     breakpoints: {
+//         768: {
+//             slidesPerView: 3.5,
+//         },
+//         767: {
+//             slidesPerView: 2,
+//         },
+//         320: {
+//             slidesPerView: 1.7,
+//         },
+//     },
+// })
