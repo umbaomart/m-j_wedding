401b5ae5b80d774fde8254fc35001f189a140ab6
dev_Mart <martumbaocommude@gmail.com>
2024-04-16 23:52:59 +0800

    add assets and section 2-5

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,113 @@
-// import './style.css'
-// import typescriptLogo from './typescript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.ts'
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `
+const swiperGroomsMenThumb = new Swiper('.swiper-groomsThumb', {
+    slidesPerView: 5,
+    freeMode: true,
+    watchSlidesProgress: true,
+})
 
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const swiperGroomsMen = new Swiper('.swiper-groomsMen', {
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 5,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    thumbs: {
+        swiper: swiperGroomsMenThumb
+    }
+})
+
+const swiperBridesMaidsThumb = new Swiper('.swiper-bridesThumb', {
+    slidesPerView: 5,
+    freeMode: true,
+    watchSlidesProgress: true,
+})
+
+const swiperBridesMaid = new Swiper('.swiper-bridesMaids', {
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 5,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    thumbs: {
+        swiper: swiperBridesMaidsThumb
+    }
+})
+
+
+const swiperInvitations = new Swiper('.swiper-invitations', {
+    slidesPerView: 1,
+    loop: true,
+    reverseDirection: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    }
+})
+
+const options = {
+    spaceBetween: 10,
+    loop: true,
+    grabCursor: true,
+    autoplay: {
+        delay: false,
+        disableOnInteraction: false,
+        waitForTransition: true,
+    },
+    speed: 20000,
+    breakpoints: {
+        768: {
+            slidesPerView: 3.5,
+        },
+        767: {
+            slidesPerView: 2,
+        },
+        320: {
+            slidesPerView: 1.7,
+        },
+    },
+}
+
+const swiperGallery = new Swiper('.gallery__slider', options)
+const swiperGallery2 = new Swiper('.gallery__slider2', options)
+
+const swiperGalleryReverse = new Swiper('.gallery__slider-reverse', {
+    direction: 'horizontal', // Set direction to horizontal
+    spaceBetween: 10,
+    loop: true,
+    grabCursor: true,
+    autoplay: {
+        delay: false,
+        disableOnInteraction: false,
+        waitForTransition: true,
+        reverseDirection: true,
+    },
+    speed: 20000,
+    breakpoints: {
+        768: {
+            slidesPerView: 3.5,
+        },
+        767: {
+            slidesPerView: 2,
+        },
+        320: {
+            slidesPerView: 1.7,
+        },
+    },
+})
